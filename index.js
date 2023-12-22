bdc786760062f1e7b2336c004dae488d6f503ee4
Aaron-Lewis-Gruberg <aaron.gruberg@dominodatalab.com>
2023-12-21 18:24:20 -0800

    added /posts/ routes to display individual posts.  need to persist data.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import _ from 'lodash';
 
 const app = express();
 const port = 3000;
@@ -10,22 +11,69 @@ const previewContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
 app.use(express.static("public/"));
 app.use(bodyParser.urlencoded({ extended: true}));
 
+var postsArray = [];
+
+// Homepage route
 app.get("/", (req, res) => {
-    res.render("index.ejs", {homeContent: homeContent, previewContent: previewContent});
+    res.render("index.ejs", {homeContent: homeContent,
+         previewContent: previewContent,
+         postsArray: postsArray});
+         //console.log(postsArray);
 });
 
+// Compose a blog post
 app.post("/compose", (req, res) => {
     res.render("create.ejs");
   });
 
+// Submit your blog post
 app.post("/submit", (req, res) => {
-    const title = req.body["title"]
-    const post = req.body["post"]
-    res.render("index.ejs", {homeContent: homeContent, 
-        previewContent: previewContent, title: title, post: post});
+    const post = [
+        req.body["title"],
+        req.body["content"]
+    ];
+    postsArray.push(post);
+    res.redirect("/");
+});
+
+// Navigate to a specific blog post
+app.get("/posts/:test", (req, res) => {
+    
+    // Convert url input text to lowercase using Lodash
+    const requestedTitle = _.lowerCase(req.params.test);
+
+    // Convert postArray title to lowercase using Lodash
+    if (postsArray != undefined) {
+        for (let i = 0; i < postsArray.length; i++) {
+
+            // Keep original title to pass to post.ejs file
+            const title = postsArray[i][0];
+
+            // Convert title to lower case
+            postsArray[i][0] =  _.lowerCase(postsArray[i][0]);
+
+            console.log(postsArray[i][0]);
+            console.log(requestedTitle);
+
+             // Check if postsArray includes the requestedTitle
+            if (postsArray[i][0].includes(requestedTitle)) {
+
+                //console.log("match found");
+
+                // Send user to selected post
+                res.render("post.ejs", {
+                    title: title, 
+                    content: postsArray[i][1]
+                });
+            }
+          }
+        
+    }
+
 });
 
 //Run app
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 });
+
